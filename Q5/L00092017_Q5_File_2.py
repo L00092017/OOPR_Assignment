d686a95e7877ee3ef1cae2870dfdc0f3708aa6ee
Malcolm Houston <L00092017@student.lyit.ie>
2021-11-08 20:55:47 +0000

    Q5 updated with additional assignment task to list file accessed times

--- Q5/L00092017_Q5_File_2.py
+++ Q5/L00092017_Q5_File_2.py
@@ -24,24 +24,30 @@ if __name__ == '__main__':
 '''
 # Import required modules
 import paramiko
+import sys
 
 
 # Define function for ssh connection and run commands
 def ssh_connection():
     try:  # attempt connection to VM
+
         # Define variables for use in the connection
         ip = "192.168.15.132"
         user_name = "l00092017".rstrip("\n")
         user_password = "l00092017".rstrip("\n")
+
         # Create the SSH connection to the VM
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
         ssh.connect(ip, username=user_name, password=user_password)
         print("Connected to: ", ip)  # Output connected status
+
         # Commands to create the folder structure
         ssh.exec_command("mkdir Labs\n")
         ssh.exec_command("mkdir Labs/lab1\n")
         ssh.exec_command("mkdir Labs/lab2\n")
+
+
         # Output the directory listing to show folders created
         com = "ls ./Labs\n"  # Command variable
         stdin, stdout, stderr = ssh.exec_command(com)  # Run command
@@ -50,6 +56,17 @@ def ssh_connection():
             output += line
         if output:
             print("Folders structure created:\n" + output)  # Print the out directory structure
+        else:
+            print("There was no output for this command")  # Error check if command returns nothing
+
+
+        # Command to find file last access time
+        stdin, stdout, stderr = ssh.exec_command("ls -l --time=atime\n")
+        output = ''  # Set output varibale to empty
+        for line in stdout.readlines():  # Populate with results of the command
+            output += line
+        if output:
+            print("Apt update:\n" + output)  # Print the out directory structure
         else:
             print("There was no output for this command")  # Error check if command returns nothing
 
@@ -71,6 +88,7 @@ def ssh_connection():
             print("Curl Installed:\n" + output)
         else:
             print("There was no output for this command")  # Error check if command returns nothing
+
 
     # Exception error if SSH fails authentication
     except paramiko.BadAuthenticationType as e:
