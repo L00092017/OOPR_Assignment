cf0b87480928e75be19d465a6f2a53564e9bc38e
Malcolm Houston <L00092017@student.lyit.ie>
2021-11-17 15:36:59 +0000

    Q4 Updated code and inpect for errors

--- Q4/L00092017_Q4_File_2.py
+++ Q4/L00092017_Q4_File_2.py
@@ -11,45 +11,73 @@
 # Available under GNU Public License (GPL)
 #-------------------------------------------------------
 """
-if __name__ == '__main__':
-    '''
-    This is the main function
-    
-    This will check for open ports in a range and return whether open
-    
-    Parameters: host
-    
-    returns: port
-    
-'''
-# Import required modules
+''' Sockets code to carry out a port scan '''
+''' Modified from:  Network Examples 1 by Ruth Lennon, LYIT'''
+
 import socket
+import subprocess
+import sys
+from datetime import datetime
 
 
-# Define function to test for open ports
-def is_port_open(host, port):
-    s = socket.socket()  # creates a new socket
+def port_scan():
+    """
+    This function will carry out a port scan on a virtual machine
+    """
+    # Clear the screen  #use clear if running in  *nix
+    subprocess.call("cls", shell=True)
 
+    # Ask for input
+    remoteserver = input("Enter a remote host to scan: ")
+    remoteserverip = socket.gethostbyname(remoteserver)
+    # Print a nice banner with information on which host we are about to scan
+    print("-" * 60)
+    print("Please wait, scanning remote host", remoteserverip)
+    print("-" * 60)
+
+    # Check what time the scan started
+    t1 = datetime.now()
+    # Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+    # We also put in some error handling for catching errors
     try:
-        s.connect((host, port))  # tries to connect to host using the ports
-    s.settimeout(0.1)  # improve speed of scan
-    except:
-    return False  # If cannot connect because port is closed, return False
-    else:
-    return True  # If the connection was established, port is open! return True
-
-    host = input("Enter the host IP Address : ")  # Get the host IP from the user
-
-    for port in range(21, 81):  # Iterate over ports, from 21 to 81
-        if is_port_open(host, port):
+        # try 1, 1025 if you have time
+        for port in range(21, 81):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remoteserverip, port))
             if port == 22:  # Return SSH if Port 22 open
                 print(f"SSH:{port} is open     ")
             elif port == 80:  # Return HTTP if Port 80 open
                 print(f"HTTP:{port} is open     ")
-            else:
-                print(f"{port} is open     ")
-        else:
-            print(f"Port {port} is closed     ")
+            elif result == 0:
+                print("Port {}:    Open".format(port))
+                sock.close()
+    except KeyboardInterrupt:
+        print("You pressed Ctrl+C")
+        sys.exit()
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
+    except socket.error:
+        print("Couldn't connect to server")
+        sys.exit()
+
+    # Checking the time again
+    t2 = datetime.now()
+    # Calculates the difference of time, to see how long it took to run the script
+    total = t2 - t1
+    # Printing the information to screen
+    print('Scanning Completed in: ', total)
 
 
-is_port_open()
+if __name__ == '__main__':
+    '''
+     This is the main function
+
+     This will check for open ports in a range and return whether open
+
+     Parameters: host, port range
+
+     returns: open ports in scan
+     
+     '''
+    port_scan()
